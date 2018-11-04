2778b3440b4f52bad2275fcc9c0998dcefa699fb
Gabor Racz <gabor@applord.co.za>
2018-11-04 13:30:24 +0200

    Initial Commit

---
+++ GranadillaAssessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GranadillaAssessment
+//
+//  Created by Gabor Racz on 2018/11/04.
+//  Copyright © 2018 Granadilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
